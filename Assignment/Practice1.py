40f5ff43f35ea590c9a8485e535f0bdf65555854
shahilsemariya <shahil.saad@gmail.com>
2026-01-27 08:26:42 +0530

    Add files via upload

---
+++ Assignment/Practice1.py
@@ -0,0 +1,17 @@
+#name="sagar"
+#print(name)
+#output is sagar
+"""
+if we want to name and age in a single line 
+"""
+#name="sagar"
+#age=20
+#print(name , age)
+#output is_____ sagar 20
+
+"""End  >>>>>>> END perimeter>>>>>>>"""
+#print("hello")
+#print("world")
+
+print("hello" , end=" miss ")
+print("world")
